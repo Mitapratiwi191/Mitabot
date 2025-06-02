7f2497d01ade6a6f980d35423370cf1e0da62591
Mitapratiwi191 <mirtaw40@gmail.com>
2025-06-02 18:25:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-const { default: makeWASocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion } = require('@adiwajshing/baileys');
-const { Boom } = require('@hapi/boom');
-const path = require('path');
-
-const { state, saveState } = useSingleFileAuthState(path.join(__dirname, 'auth_info.json'));
+const { default: makeWASocket, useSingleFileAuthState } = require('@adiwajshing/baileys');
+const { state, saveState } = useSingleFileAuthState('./auth_info.json');
 
 async function startBot() {
-  const { version, isLatest } = await fetchLatestBaileysVersion();
-  console.log(`Using WA version v${version.join('.')}, isLatest: ${isLatest}`);
+    const sock = makeWASocket({
+        auth: state
+    });
 
-  const sock = makeWASocket({
-    version,
-    printQRInTerminal: true,
-    auth: state
-  });
+    sock.ev.on('creds.update', saveState);
 
-  sock.ev.on('connection.update', (update) => {
-    const { connection, lastDisconnect } = update;
-    if(connection === 'close') {
-      const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut;
-      console.log('connection closed due to', lastDisconnect.error, ', reconnecting:', shouldReconnect);
-      if(shouldReconnect) {
-        startBot();
-      }
-    } else if(connection === 'open') {
-      console.log('connected');
-    }
-  });
+    sock.ev.on('connection.update', (update) => {
+        console.log(update);
+        if (update.connection === 'close') {
+            console.log('Connection closed, reconnecting...');
+            startBot();
+        }
+    });
 
-  sock.ev.on('creds.update', saveState);
+    sock.ev.on('messages.upsert', ({ messages }) => {
+        console.log('New message:', messages);
+    });
 }
 
 startBot();
+
 
 
 
